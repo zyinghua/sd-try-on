6776c62b83a84a079f26c22ddeec5f0e48bb60dd
zyinghua <49605250+zyinghua@users.noreply.github.com>
2026-04-22 17:12:02 -0400

    add resume sh

--- run_scripts/train_sdtryon_clip_resampler_resume.sh
+++ run_scripts/train_sdtryon_clip_resampler_resume.sh
@@ -3,11 +3,11 @@
 # source /etc/network_turbo
 # export HF_HUB_DISABLE_XET=1
 
-accelerate launch train_control.py \
+accelerate launch train_clip_resampler.py \
     --pretrained_model_name_or_path "Manojb/stable-diffusion-2-1-base" \
     --image_encoder_path "patrickjohncyh/fashion-clip" \
     --conditioning_channels 3 \
-    --train_data_dir "/root/autodl-tmp/viton-hd-dataset" \
+    --train_data_dir "/root/autodl-tmp/data/sdtryon/train" \
     --output_dir "/root/autodl-tmp/sdtryon-model" \
     --num_train_epochs 1 \
     --max_train_steps 50000 \
@@ -19,7 +19,9 @@ accelerate launch train_control.py \
     --checkpoints_total_limit 20 \
     --validation_steps 500 \
     --validation_prompt "A female wearing a white T-shirt with a red LEVI's logo." "A female wearing a black T-shirt with white logo and text." \
-    --validation_image "/root/autodl-tmp/data/sdtryon/train/image/00000_00.jpg" "/root/autodl-tmp/data/sdtryon/train/image/00019_00.jpg" \
+    --validation_image "/root/autodl-tmp/data/sdtryon/train/image-densepose/00000_00.jpg" "/root/autodl-tmp/data/sdtryon/train/image-densepose/00019_00.jpg" \
     --validation_cloth_image "/root/autodl-tmp/data/sdtryon/train/cloth/00000_00.jpg" "/root/autodl-tmp/data/sdtryon/train/cloth/00019_00.jpg" \
     --ip_adapter_image_drop_rate 0.05 \
+    --cloth_id_loss_weight 0.15 \
+    --resume_from_checkpoint "/root/autodl-tmp/sdtryon-model/checkpoint-20000" \
     #--pretrained_ip_adapter_path "/path/to/pretrained_ip_adapter.bin" \
